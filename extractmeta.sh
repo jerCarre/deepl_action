7d4140ef9947489a12ba2f7929d593752f230b3b
jerCarre <22233742+jerCarre@users.noreply.github.com>
2022-04-28 11:24:13 +0200

    Add files via upload

---
+++ extractmeta.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+OUTPUT=""
+INPUT=""
+
+if (($# > 1)); then
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+        -o|--output)
+            OUTPUT="$2"
+            shift
+            shift
+            ;;
+        *)
+            INPUT="$1"
+            shift
+    esac
+    done
+else if (($# == 1)); then
+       INPUT=$1
+     else
+       echo "usage: ..."
+     fi
+fi
+
+echo '$meta-json$' > /tmp/metadata.pandoc-tpl
+
+if [ -z "$OUTPUT" ]; then
+  pandoc --template=/tmp/metadata.pandoc-tpl $INPUT
+else
+  pandoc --template=/tmp/metadata.pandoc-tpl $INPUT > $OUTPUT
+fi
+rm -f /tmp/metadata.pandoc-tpl
